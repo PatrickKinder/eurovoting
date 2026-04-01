051325ea48131ee8ae273a8c725b341e96175e5a
PatrickKinder <p13kindermann@gmail.com>
2026-04-01 21:43:27 +0200

    initial project setup

---
+++ src/main/java/com/eurovoting/eurovoting/EurovotingApplication.java
@@ -0,0 +1,13 @@
+package com.eurovoting.eurovoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurovotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurovotingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eurovoting/eurovoting/EurovotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurovoting.eurovoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurovotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
